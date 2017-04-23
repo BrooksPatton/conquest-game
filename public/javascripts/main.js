9b28dcabcff49bf9a057d82c7744ef12cb2f6128
Brooks Patton <scgryphon@sbcglobal.net>
2017-04-22 19:21:36 -0600

    about to make some changes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,33 @@
-console.log('yo')
+function createMap(size) {
+    var cellId = 0
+
+    for(var i = 0; i < size; i++) {
+        var $row = $('<div>')
+
+        $row.addClass('row')
+
+        for(var j = 0; j < size; j++) {
+            var $cell = $('<div>')
+
+            $cell.addClass('cell')
+            $cell.addClass('water')
+            $cell.attr('id', cellId)
+            $cell.attr('data-id', cellId)
+
+            $row.append($cell)
+
+            cellId++
+        }
+
+        $('main').append($row)
+    }
+}
+
+createMap(25)
+
+$('main').on('mouseover', '.cell', function(e) {
+    var $cell = $(this)
+
+    $cell.removeClass('water')
+    $cell.addClass('land')
+})
