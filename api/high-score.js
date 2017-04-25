f048300b234dbfa49b6e82cef4ce80b58d52f7c6
Brooks Patton <scgryphon@sbcglobal.net>
2017-04-24 20:53:00 -0600

    :bug: added validation and a secret code. It also plays games to not break old versions of the game

--- api/high-score.js
+++ api/high-score.js
@@ -5,18 +5,50 @@ const router = express.Router()
 
 router.get('/', (req, res, next) => {
   db.getAll()
-  .then(data => res.json(data))
+  .then(data => {
+    if(data.length < 10) {
+      for(let i = data.length; i < 10; i++) {
+        data[i] = {score: 0}
+      }
+    }
+
+    res.json(data)
+  })
   .catch(err => next(err))
 })
 
-router.post('/', (req, res, next) => {
-  req.body.score = +req.body.score
+router.post('/', validator, prepScore, (req, res, next) => {
+  if(!req.ok) {
+    console.error('validation failed')
+    return res.sendStatus(200)
+  }
 
-  console.log(req.body)
+  req.body.score = +req.body.score
 
   db.add(req.body)
   .then(data => res.json(data))
   .catch(err => next(err))
 })
 
+function validator(req, res, next) {
+  const code = process.env.SECRET_CODE || 'supersecretcode'
+  let ok = true
+
+  if(!req.body.code) ok = false
+  if(req.body.code !== code) ok = false
+  if(!req.body.score) ok = false
+
+  req.ok = ok
+
+  next()
+}
+
+function prepScore(req, res, next) {
+  req.body.score = Number(req.body.score)
+
+
+  if(isNaN(req.body.score)) req.ok = false
+
+  next()
+}
 module.exports = router
